2a515a63acb5ab6449dfbef340d1e1df63de007d
web-test <alypwr573@gmail.com>
2024-09-16 06:29:02 -0700

    اتمام دکوریتور و تمرین آن

--- decorator.py
+++ decorator.py
@@ -1,12 +1,11 @@
-COUNTRYS = ['Iran', 'UAE']
-VAT = {'iran':9,'UAE':15}
-def show_total_price(purchase):
-    pass
-def show_vat_pluse_price(purchase) :
+COUNTRIES = ["Iran","UAE"]
+VAT={"Iran":9,"UAE":15}
+
+
+class User :
     pass
 
-class User:
-    pass
+
 
 class Address:
     def __init__(self, country):
@@ -20,32 +19,39 @@ class Product:
 class Purchase:
     def __init__(self, user, address):
         self.user = user 
-        self.product_list = []
         self.address = address
+        self.product_list =[]
 
-    def add_products(self,product_list):
-        if not isinstance(product_list,list):
-            product_list = [product_list]
-        self.product_list.extend(product_list)
+    def add_products(self,products_list):
+        if not isinstance(products_list,list):
+           products_list = [products_list] 
+        self.product_list.extend(products_list)
 
     def total_price(self):
-        s = 0
+        s=0
         for product in self.product_list:
-            s += product.price
-
+            s+=product.price
         return s
-    
+def calculate_vat(func):
+    def wrapped_func(pur):
+        vat = VAT[pur.address.country]
+        total_price = pur.total_price()
+        return total_price +total_price * 9 / 100
+    return wrapped_func
 
+def show_total_price(p):
+    return p.total_price()
+@calculate_vat
+def show_vat_pluse_price(p):
+    return p.total_price()
 if __name__ == "__main__":
     user = User()
-    addr_iran = Address(country=COUNTRYS[0])
-    addr_UAE = Address(country=COUNTRYS[1])
-    p1 = Product("persian rugs",150)
-    p2 = Product("nain rugs", 160)
-    product = [p1, p2]
-
-    ph = Purchase(user=user , address=addr_UAE )
-    ph.add_products(p1)
-    ph.add_products([p2,p1])
-    print(ph.total_price())
-
+    address1 =Address(country=COUNTRIES[0])
+    address2 =Address(country=COUNTRIES[1])
+    p1= Product("rug",150)
+    p2= Product("television",300)
+    p3= Product("seshvar",300)
+    purchase = Purchase(user=user,address=address1)
+    purchase.add_products([p1,p2])
+    print(show_total_price(purchase))
+    print(show_vat_pluse_price(purchase))
--- test.py
+++ test.py
@@ -1,50 +1,55 @@
-from abc import ABC , abstractmethod
-
-class Car(ABC):
-    @abstractmethod
-    def drive(self):
-        pass
-
-class Motorcycle(ABC):
-    @abstractmethod
-    def ride(self):
-        pass
-
-
-class Seban(Car):
-    def drive(self):
-        print("sedan is driving")
-
-class SUV(Car):
-    def drive(self):
-        print("SUV is driving")
-
-
-class SportBike(Motorcycle):
-    def ride(self):
-        print("Riding a SportBike")
-
-
-class CreateFactory(ABC):
-    @abstractmethod
-    def car_creator(car_name):
-        if car_name =="Seban":
-            return Seban()
-        elif car_name == "SUV":
-            return SUV()
-        else:
-            print( InvalidValue(f"name {car_name} Invalid!!!")) 
-    def motor_creator(self,motor_name):
-        if motor_name =="Seban":
-            return Seban()
-        elif motor_name == "SUV":
-            return SUV()
-        else:
-            return InvalidValue(f"name {motor_name} Invalid!!!")
-
-class InvalidValue(Exception):
+DATA = {"Iran":9,"UAE":15}
+COUNTRIES = list(DATA.keys())
+class User:
     pass
 
+class Address:
+    def __init__(self, country):
+        self.country = country
 
-if __name__ == "__main__":
-    C_suv = CreateFactory.car_creator(car_name="UV")
+class Purchase:
+    def __init__(self, user, address):
+        self.user = user
+        self.address = address
+        self.product_list = []
+
+    def add_products(self,products_list):
+        if not isinstance(products_list,list):
+           products_list = [products_list] 
+        self.product_list.extend(products_list)
+    
+    def total_price(self):
+        s = 0
+        for product in self.product_list:
+            s+=product.price
+        return s
+
+class Product:
+    def __init__(self, name, price):
+        self.name = name 
+        self.price = price
+def calculate_vat(func):
+    def wrapper_func(pur):
+        vat = DATA[pur.address.country]
+        total_price = pur.total_price()
+        return total_price +total_price * vat /100
+    return wrapper_func
+
+@calculate_vat
+def show_price_plus_vat(p):
+    return p.total_price()
+
+def show_price(p):
+    return p.total_price()
+if __name__  =="__main__":
+    
+    user =User()
+    address_iran = Address(country=COUNTRIES[0])
+    address_uae = Address(country=COUNTRIES[1])
+    pr1= Product("pr1",150)
+    pr2= Product("pr2",326)
+    pr3= Product("pr3",178)
+    purchase = Purchase(user=user,address=address_iran)
+    purchase.add_products([pr1, pr2, pr3])
+    print(show_price_plus_vat(purchase))
+    print(show_price(purchase))
